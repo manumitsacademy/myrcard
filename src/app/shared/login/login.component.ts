c23b452fda92e40dcd44bb492bee4aff82e5a258
manumitsacademy <manumits.info@gmail.com>
2020-03-26 20:18:01 +0530

    'hexagon'

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +11,19 @@ import { AuthService } from 'src/app/auth/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public authService:AuthService,public router:Router) { }
-
+  constructor(public authService:AuthService,public router:Router,public http:HttpClient,public fb:FormBuilder) {
+    this.loginForm = this.fb.group({
+      email:[],
+      password:[]
+    })
+  }
+  
+  loginForm:FormGroup;
   ngOnInit() {
     
+  }
+  login(){
+    this.http.post("https://revcard.herokuapp.com/api/v1/loginUser",this.loginForm.value)
+    .subscribe((res)=>{console.log("Login:::::::",res)})
   }
 }
