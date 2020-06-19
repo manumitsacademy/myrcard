26e0950a02c97f9f7b1b1846cb80212868bfb8fc
Praveen-at-289810389200 <manumits.info@gmail.com>
2020-06-19 13:37:28 +0530

    nodesoaptest

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const path = require("path");
 const express = require("express");
 const app = express();
 app.use(express.static(__dirname + '/angular-build'));
+app.get("/abc",function(){
+    res.send("<h1>Its ok</h1>")
+})
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname+'/angular-build'+'/index.html'))
+    res.sendFile(path.join(__dirname+'/angular-build'+'/index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
