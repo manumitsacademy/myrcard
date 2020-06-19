4ea8efa89f2cc08c638a4c536d945841d0000e13
Praveen-at-289810389200 <manumits.info@gmail.com>
2020-06-19 13:57:14 +0530

    nodesoaptest1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 app.use(express.static(__dirname + '/angular-build'));
-app.get("/abc",function(){
+app.get("/abc",function(req,res){
     res.send("<h1>Its ok</h1>")
 })
 app.get('/*', function(req,res){
