2ef64d5d6486ba6cc8a6b918441b8e8a6db9c215
Praveen-at-289810389200 <manumits.info@gmail.com>
2020-06-19 19:43:16 +0530

    soapservice1

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const soapRequest = require("easy-soap-request");
 app.use(express.static(__dirname + '/angular-build'));
-app.get("/abc",function(req,res){
-    res.send("<h1>Its ok</h1>")
+app.get("/abc",function (req, res, next) {
+    // example data
+    const url = "https://revcard.pearlcapital.com:7073";
+    const sampleHeaders = {
+        "Content-Type": "text/xml;charset=UTF-8",
+        "Authorization": "UmV2ZW51ZWRIZXJva3VTaXRlL0ZJNzhKSkNSMzRXOTAhNTY=",
+		"soapAction": 'https://revcard.pearlcapital.com:7073/Revenued.wsdl#RevenuedTest',
+    };
+
+    soapRequest({
+        url: url,
+        headers: sampleHeaders
+    }).then((result) => {
+        console.log("res",result);
+        res.send("chiddam");
+    }).catch((err) => {
+        console.log("err", err.toJSON());
+        res.sendStatus(500);
+    });
 })
 app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname+'/angular-build'+'/index.html'))
