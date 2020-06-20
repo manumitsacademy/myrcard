164dc967f4aeb962ddfbd1ae091685a355d11f3d
Praveen-at-289810389200 <manumits.info@gmail.com>
2020-06-20 18:19:04 +0530

    'soapapis'

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 var soap = require('strong-soap').soap;
 app.use(express.static(__dirname + '/angular-build'));
-app.get("/account", function (req, res, next) {
+app.get("/accountsummary", function (req, res, next) {
     var url = "https://revcard.pearlcapital.com:7073/Revenued.wsdl";
 
     var requestArgs = {
@@ -20,6 +20,28 @@ app.get("/account", function (req, res, next) {
             "Authorization":"UmV2ZW51ZWRIZXJva3VTaXRlL0ZJNzhKSkNSMzRXOTAhNTY="
         };
         var method = client['Revenued']['RevenuedSoap']['RevenuedGetAcctSummary'];
+        method(requestArgs, function (err, result, envelope, soapHeader) {
+            res.send(result);
+        }, null , customRequestHeader);
+    });
+});
+app.get("/transactionhistory", function (req, res, next) {
+    var url = "https://revcard.pearlcapital.com:7073/Revenued.wsdl";
+
+    var requestArgs = {
+        oppId: '0060y00001D8L1dAAF',
+        sysDate: '5/29/2020',
+        sessionId: '?'
+    };
+
+    var options = {};
+
+    soap.createClient(url, options, function (err, client) {
+        var customRequestHeader = {
+            "Content-Type": "text/xml;charset=UTF-8",
+            "Authorization":"UmV2ZW51ZWRIZXJva3VTaXRlL0ZJNzhKSkNSMzRXOTAhNTY="
+        };
+        var method = client['Revenued']['RevenuedSoap']['RevenuedGetTrxnHistory'];
         method(requestArgs, function (err, result, envelope, soapHeader) {
             res.send(result);
         }, null , customRequestHeader);
