791c132621b97be5625c3285e9149f50edcd2de4
Praveen-at-289810389200 <manumits.info@gmail.com>
2020-06-20 18:05:48 +0530

    'strong-soap'

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const soapRequest = require("easy-soap-request");
+var soap = require('strong-soap').soap;
 app.use(express.static(__dirname + '/angular-build'));
-app.get("/abc",function (req, res, next) {
-    // example data
-    const url = "https://revcard.pearlcapital.com:7073";
-    const sampleHeaders = {
-        "Content-Type": "text/xml;charset=UTF-8",
-        "Authorization": "UmV2ZW51ZWRIZXJva3VTaXRlL0ZJNzhKSkNSMzRXOTAhNTY=",
-		"soapAction": 'https://revcard.pearlcapital.com:7073/Revenued.wsdl#RevenuedTest',
+app.get("/account", function (req, res, next) {
+    var url = "https://revcard.pearlcapital.com:7073/Revenued.wsdl";
+
+    var requestArgs = {
+        oppId: '0060y00001D8L1dAAF',
+        sysDate: '5/29/2020',
+        sessionId: '?'
     };
 
-    soapRequest({
-        url: url,
-        headers: sampleHeaders
-    }).then((result) => {
-        console.log("res",result);
-        res.send(JSON.stringify(result));
-    }).catch((err) => {
-        console.log("err", err.toJSON());
-        res.sendStatus(500);
+    var options = {};
+
+    soap.createClient(url, options, function (err, client) {
+        var customRequestHeader = {
+            "Content-Type": "text/xml;charset=UTF-8",
+            "Authorization":"UmV2ZW51ZWRIZXJva3VTaXRlL0ZJNzhKSkNSMzRXOTAhNTY="
+        };
+        var method = client['Revenued']['RevenuedSoap']['RevenuedGetAcctSummary'];
+        method(requestArgs, function (err, result, envelope, soapHeader) {
+            res.send(result);
+        }, null , customRequestHeader);
     });
-})
+});
 app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname+'/angular-build'+'/index.html'))
 });
