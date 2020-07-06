5360ac8d03d69fda0bc14242842c51efc6a4561c
Praveen-at-289810389200 <manumits.info@gmail.com>
2020-07-06 13:26:25 +0530

    'herokuvars'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   buildname:'its in production',
-  soapBase:'https://praveeng-1002.herokuapp.com/'
+  soapBase:'https://praveeng-1002.herokuapp.com/',
+  authURL:process.env.authURL
 };
