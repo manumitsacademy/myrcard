b17c23d5040be9f4018a8e9b71d97991aba2aaad
Praveen-at-289810389200 <manumits.info@gmail.com>
2020-08-11 17:45:29 +0530

    crs

--- server.js
+++ server.js
@@ -5,35 +5,17 @@ const http = require('http');
 var bodyParser = require('body-parser')
 var soap = require('strong-soap').soap;
 var logger = require('./logger')
-var cors = require('cors')
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json());
-
-var allowedOrigins = ['http://localhost:4200','http://localhost:8080/',
-'https://praveeng-1002-herokuapp.com'];
 app.use(express.static(__dirname + '/angular-build'));
-//app.use(cors());
-// app.use(cors({
-// origin: function(origin,callback){
-//     if(allowedOrigins.includes(origin)){
-//         return callback(null,true)
-//     }
-//     else{
-//         return callback('not allowed',false)
-//     }
-// }
-// }));
+var authUrl = process.env.authURL || "https://revcard.herokuapp.com/api/";   
+var url = process.env.Url || "https://revcard.pearlcapital.com:7073/Revenued.wsdl";
+var date = new Date();
+var sysDate = date.getTime()-(24*60*60*1000);
 
-var authUrl = process.env.authURL || "https://revcard.herokuapp.com/api/";
-
-
-    
-    var url = process.env.Url || "https://revcard.pearlcapital.com:7073/Revenued.wsdl";
-    var date = new Date();
-    var sysDate = date.getTime()-(24*60*60*1000);
-app.get("/getAuthUrl",cors(),(req,res)=>{
+app.get("/getAuthUrl",(req,res)=>{
     logger.info('welcome to loggeer')
     res.send({authUrl:authUrl})
 })
@@ -70,7 +52,7 @@ app.use((req,res,next)=>{
     }
 })
 
-app.get("/accountsummary/:oppId",cors(), function (req, res, next) {
+app.get("/accountsummary/:oppId",function (req, res, next) {
     var requestArgs = {
         oppId: req.params.oppId,
         sysDate,
@@ -119,7 +101,7 @@ app.get("/transactionhistory/:oppId", function (req, res, next) {
         }
     });
 });
-app.get('/*',cors(), function(req,res){
+app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname+'/angular-build'+'/index.html'))
 });
 // Start the app by listening on the default Heroku port
