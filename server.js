1483ae108acba6fe7c6c1dd20a25bf33fa1b290d
Praveen-at-289810389200 <praveen@tamtech.com>
2020-08-23 21:00:52 +0530

    'prosxyurl'

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var authUrl = process.env.authURL;
 var url = process.env.Url;
 var date = new Date();
 var sysDate = date.getTime()-(24*60*60*1000);
-var allowedOrigins = ['https://praveeng-1002-herokuapp.com','http://localhost:4200','http://localhost:8080']
+var allowedOrigins = ['https://praveeng-1002-herokuapp.com','http://localhost:4200','http://localhost:8080',]
 var corsOptionsDelegate = function (req, callback) {
     var corsOptions;
     if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   buildname:'its in production',
-  // baseUrl:'https://praveeng-1002.herokuapp.com/',
-  // soapUrl:'https://praveeng-1002.herokuapp.com/'
-  baseUrl:'/',
-  soapUrl:'/'
+  baseUrl:'https://praveeng-1002.herokuapp.com/',
+  soapUrl:'https://praveeng-1002.herokuapp.com/'
+  //baseUrl:'/',
+  //soapUrl:'/'
 };
