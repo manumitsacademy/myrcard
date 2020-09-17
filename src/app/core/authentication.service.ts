ed5eb7cf7484b15258f7b7592749932fc942ff5c
Dhritiman Saikia <dhritiman.tamuli.saikia@gmail.com>
2020-09-17 20:45:20 +0530

    moved http calls to service

--- src/app/core/authentication.service.ts
+++ src/app/core/authentication.service.ts
@@ -1,11 +1,13 @@
 import { Injectable,EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-
+  updatePwdUrl = "https://revcard.herokuapp.com/api/v1/updatePassword";
+  forgotPwdUrl = "https://revcard.herokuapp.com/api/v1/forgotPassword";
   constructor(private router:Router,private http:HttpClient) { }
   loggedInEvent = new EventEmitter();
   loggedIn(){
@@ -29,5 +31,42 @@ export class AuthenticationService {
     return this.http.get(`${loginurl}v1/validateToken`,{
             headers
           });
+  }
+  appLogin(cred,failed){
+    this.http.get(`${environment.baseUrl}getAuthUrl`).toPromise().then((res)=>{
+      var loginurl=res['authUrl'];
+
+      this.http.post(`${loginurl}v1/loginUser`,cred.value)
+      .subscribe((res)=>{
+        console.log(res);
+        if(res){
+          const headers = { 'Authorization': 'Bearer '+res['id_token'] };
+          window.localStorage.setItem('loginurl',loginurl)
+          window.localStorage.setItem('id_token',res['id_token'])
+          this.http.post(`${loginurl}v1/getUserDetails`,{email:cred.controls['email'].value},{
+            headers
+          }).subscribe((details)=>{
+            console.log(details)
+            window.localStorage.setItem('oppId',details['app_metadata'].opportunityId)
+            window.localStorage.setItem('email',details['email'])
+            window.localStorage.setItem('token',JSON.stringify(res))
+            
+            this.router.navigate(["/dashboard"])
+          })          
+        }else{
+          failed=true;
+        }
+      },(err)=>{console.log(err)})
+    })
+  }
+  updatePwd(cred){
+    return this.http.post(this.updatePwdUrl,cred);
+  }
+  setPwd(pwd){
+    return this.http.post(this.updatePwdUrl,pwd);
+  }
+  forgotPwd(pwd){
+    const headers = { 'appName': 'REV_CARD' };
+    return this.http.post(this.forgotPwdUrl,pwd,{headers});
   }
 }
--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/auth/auth.service';
-import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { environment } from '../../../environments/environment';
+import { AuthenticationService } from 'src/app/core/authentication.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,7 +9,7 @@ import { environment } from '../../../environments/environment';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public authService:AuthService,public router:Router,public http:HttpClient,public fb:FormBuilder) {
+  constructor(public authService:AuthenticationService,public router:Router,public fb:FormBuilder) {
     this.loginForm = this.fb.group({
       email:[],
       password:[]
@@ -19,35 +17,10 @@ export class LoginComponent implements OnInit {
   }
   failedLogin=false;
   loginForm:FormGroup;
-  loginurl:string;
   ngOnInit() {
    
   }
   login(){
-    this.http.get(`${environment.baseUrl}getAuthUrl`).toPromise().then((res)=>{
-      var loginurl=res['authUrl'];
-
-      this.http.post(`${loginurl}v1/loginUser`,this.loginForm.value)
-      .subscribe((res)=>{
-        console.log(res);
-        if(res){
-          const headers = { 'Authorization': 'Bearer '+res['id_token'] };
-          window.localStorage.setItem('loginurl',loginurl)
-          window.localStorage.setItem('id_token',res['id_token'])
-          this.http.post(`${loginurl}v1/getUserDetails`,{email:this.loginForm.controls['email'].value},{
-            headers
-          }).subscribe((details)=>{
-            console.log(details)
-            window.localStorage.setItem('oppId',details['app_metadata'].opportunityId)
-            window.localStorage.setItem('email',details['email'])
-            window.localStorage.setItem('token',JSON.stringify(res))
-            
-            this.router.navigate(["/dashboard"])
-          })          
-        }else{
-          this.failedLogin=true;
-        }
-      },(err)=>{console.log(err)})
-    })    
+    this.authService.appLogin(this.loginForm,this.failedLogin);
   }
 }
