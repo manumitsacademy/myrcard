5c91b6afb2fd070043124450ce99130179c58466
Dhritiman Saikia <dhritiman.tamuli.saikia@gmail.com>
2020-10-26 12:52:08 +0530

    added test cases

--- src/app/dashboard/ministatement/ministatement.component.spec.ts
+++ src/app/dashboard/ministatement/ministatement.component.spec.ts
@@ -2,6 +2,8 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { of } from 'rxjs/internal/observable/of';
 import { AccountService } from 'src/app/core/account.service';
 import transactions from 'src/app/core/model/mock-transaction-history.json';
@@ -13,6 +15,7 @@ describe('MinistatementComponent', () => {
   let fixture: ComponentFixture<MinistatementComponent>;
   let mockAccountService;
   let trans;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     trans = transactions;
@@ -32,24 +35,23 @@ describe('MinistatementComponent', () => {
     fixture = TestBed.createComponent(MinistatementComponent);
     component = fixture.componentInstance;
     //fixture.detectChanges();
+    debugElement = fixture.debugElement;
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  it('recent transactions table should be present', () => {
+    const table = debugElement.query(By.css('.transac-table'));
+    expect(table).toBeTruthy();
+  });
+
   it('should return transaction history', () => {
-    mockAccountService.getTransactionHistory.and.returnValue(of(trans));
+    mockAccountService.getTransactionHistory.and.returnValue(of(JSON.stringify(trans)));
     fixture.componentInstance.transactionHistory = trans;
     fixture.detectChanges();
-    expect(trans.length).toBe(5);
-    console.log(fixture.componentInstance.transactionHistory);
-    let abc = fixture.componentInstance.transactionHistory;
-    console.log(abc[0]);
-    // component.ngOnInit();
-    // component.data.subscribe((res) => {
-    //   console.log(res);
-    //   expect(res).toEqual([fakeData]);
-    // })
+    expect(trans.Result.array.RevTrxn.length).toBe(5);
   });
 });
