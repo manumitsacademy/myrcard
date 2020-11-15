b3b1f9a8bf9065e039e88586f767404e7d05278c
Dhritiman Saikia <dhritiman.tamuli.saikia@gmail.com>
2020-11-15 10:55:56 +0530

    refactored code

--- src/app/dashboard/shortcuts/shortcuts.component.spec.ts
+++ src/app/dashboard/shortcuts/shortcuts.component.spec.ts
@@ -1,35 +1,35 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { Router } from '@angular/router';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { AuthenticationService } from 'src/app/core/authentication.service';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationService } from 'src/app/core/authentication.service';
 
-// import { ShortcutsComponent } from './shortcuts.component';
+import { ShortcutsComponent } from './shortcuts.component';
 
-// describe('ShortcutsComponent', () => {
-//   let component: ShortcutsComponent;
-//   let fixture: ComponentFixture<ShortcutsComponent>;
-//   let mockAuthService;
-//   beforeEach(async(() => {
-//     mockAuthService = jasmine.createSpyObj(['signOut']);
-//     TestBed.configureTestingModule({
-//       declarations: [ShortcutsComponent],
-//       providers: [
-//         { provide: AuthenticationService, useValue: mockAuthService },
+describe('ShortcutsComponent', () => {
+    let component: ShortcutsComponent;
+    let fixture: ComponentFixture<ShortcutsComponent>;
+    let mockAuthService;
+    beforeEach(async(() => {
+        mockAuthService = jasmine.createSpyObj(['signOut']);
+        TestBed.configureTestingModule({
+            declarations: [ShortcutsComponent],
+            providers: [
+                { provide: AuthenticationService, useValue: mockAuthService },
 
-//       ],
-//       imports: [RouterTestingModule],
-//     })
-//       .compileComponents();
-//   }));
+            ],
+            imports: [RouterTestingModule],
+        })
+            .compileComponents();
+    }));
 
-//   beforeEach(() => {
+    beforeEach(() => {
 
-//     fixture = TestBed.createComponent(ShortcutsComponent);
-//     component = fixture.componentInstance;
-//     //fixture.detectChanges();
-//   });
+        fixture = TestBed.createComponent(ShortcutsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
